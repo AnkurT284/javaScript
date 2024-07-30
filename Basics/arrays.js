556f9929a5f504db62faf7386cd56d9fef4cbd87
AnkurT284 <122141255+AnkurT284@users.noreply.github.com>
2024-07-30 20:24:20 +0000

    done

---
+++ Basics/arrays.js
@@ -0,0 +1,17 @@
+let myArr = [1,2,3,4,5] //declaring an array
+console.log(myArr) 
+ 
+let myArr2 = myArr
+myArr2.push(6)     //pushing an element in an array
+console.log(myArr2)
+
+myArr2.findIndex(3)
+console.log(myArr2)
+
+myArr2.join()
+console.log(myArr2)
+
+myArr2.slice(0,3)
+console.log(myArr2)
+
+myArr2.splice(0,2)
